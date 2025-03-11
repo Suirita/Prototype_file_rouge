27b4298f92995659d7c4959118002c2f673d6933
Suirita Fahd <suirita.fahd.solicode@gmail.com>
2025-03-11 09:36:55 +0000

    Policy class

---
+++ Policy_class_liveCoding/lang/fr/message.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'list_articles' => 'Liste des Articles',
+    'home' => 'Accueil',
+    'articles' => 'Articles',
+    'add_article' => 'Ajouter article',
+    'table_articles' => 'Table des Articles',
+    'number' => '#',
+    'title' => 'Titre',
+    'category' => 'Catégorie',
+    'user' => 'Utilisateur',
+    'post_date' => 'Date de publication',
+    'actions' => 'Actions',
+    'view' => 'Voir',
+    'edit' => 'Modifier',
+    'delete' => 'Supprimer',
+];
