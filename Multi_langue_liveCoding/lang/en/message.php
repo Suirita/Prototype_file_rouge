07c583cc922bd5c72df5b2ae777989022a94171d
Suirita Fahd <suirita.fahd.solicode@gmail.com>
2025-03-12 09:38:02 +0000

    Policy_class

--- Multi_langue_liveCoding/lang/en/message.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'Dashboard' => 'Dashboard',
-    'Hello world!' => 'Hello world!',
-    'You\'re logged in!' => 'You\'re logged in!',
-];
--- Multi_langue_liveCoding/lang/it/message.php
+++
@@ -1,5 +0,0 @@
-<?php
-return [
-    'Dashboard' => 'Pannello di controllo',
-    'You\'re logged in!' => 'Sei loggato!',
-];
